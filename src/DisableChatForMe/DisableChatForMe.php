d18c79a9d72fcd6553b37770dbb22538167c7a0a
Ryan James <rj0495@gmail.com>
2015-09-28 20:32:39 +0100

    Update DisableChatForMe.php

--- src/DisableChatForMe/DisableChatForMe.php
+++ src/DisableChatForMe/DisableChatForMe.php
@@ -24,57 +24,17 @@ class DisableChatForMe extends PluginBase implements Listener{
         $this->getLogger()->info("[DisableChatForMe] Successfully disabled!");
     }
 
-    /**
-     * @param CommandSender $sender
-     * @param Command $command
-     * @param string $label
-     * @param array $args
-     * @return bool|void
-     */
-
-    public function onCommand(CommandSender $sender, Command $command,$label,array $args){
-        switch($command->getName()){
-            case "chaton":{
-                $this->chaton($sender);
-                $sender->sendMessage("[DisableChat] You successfully enabled chat for yourself");
-                break;
-            }
-            case "chatoff":{
-                $this->chatoff($sender);
-                $sender->sendMessage("[DisableChat] You successfully disabled chat for yourself");
-                break;
-            }
-        }
-    }
-
-    public function ischatoff(Player $player){
-        return in_array($player->getName(),$this->players);
-    }
-
-    public function chaton(Player $player){
-        unset($this->players[$player->getName()]);
-        $player->setNameTag($player->getDisplayName());
-    }
-
-    public function chatoff(Player $player){
-        $this->players[$player->getName()] = $player->getName();
-        $player->setNameTag($player->getDisplayName()." [MUTE]");
-    }
-
     public function onChat(PlayerChatEvent $event){
         $player = $event->getPlayer();
         if($player instanceof Player){
             if(!$event->isCancelled()){
                 $user = $player->getName();
                 $message = $event->getMessage();
-                $format = "<".$user."> ".$message;
-                $this->getLogger()->info($format);
-                foreach($this->getServer()->getOnlinePlayers() as $p){
-                    if(!($this->ischatoff($p))){
-                        $p->sendMessage($format);
-                    }
+                $chat = Utils::getURL("URL...");
+                if($chat === 1){
+                $event->setCancelled(true);
                 }
-                $event->setCancelled();
+                //$event->setCancelled();
             }
         }
     }
